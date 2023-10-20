57022c2e4459f85f374712b6678d317d90352153
Mark Loose <m.loose@boewa.de>
2023-10-20 16:42:33 +0200

    Create type/schema boilerplate for jsonld + hydra

---
+++ client/types/hydra.ts
@@ -0,0 +1,31 @@
+import { AnyZodObject, z } from "zod";
+
+export const jsonLdMetaSchema = z.object({
+  "@id": z.string(),
+  "@type": z.string(),
+});
+
+export type JsonLdMeta = z.infer<typeof jsonLdMetaSchema>;
+
+export const hydraViewSchema = jsonLdMetaSchema.extend({
+  "hydra:first": z.string(),
+  "hydra:last": z.string(),
+  "hydra:next": z.string().optional(),
+});
+
+export type HydraView = z.infer<typeof hydraViewSchema>;
+
+export interface HydraCollection<T> extends JsonLdMeta {
+  "@context": string;
+  "hydra:totalItems": number;
+  "hydra:member": Array<T & JsonLdMeta>;
+  "hydra:view": HydraView;
+}
+
+export function createHydraCollectionSchema<T extends AnyZodObject>(schema: T) {
+  return jsonLdMetaSchema.extend({
+    "hydra:totalItems": z.number(),
+    "hydra:member": z.array(jsonLdMetaSchema.merge(schema)),
+    "hydra:view": hydraViewSchema,
+  });
+}
---
+++ client/types/users.ts
@@ -0,0 +1,24 @@
+import { z } from "zod";
+
+import { createHydraCollectionSchema } from "./hydra";
+
+export const userResponseSchema = z.object({
+  username: z.string(),
+  email: z.string(),
+});
+
+export type UserResponse = z.infer<typeof userResponseSchema>;
+
+export const userCollectionResponseSchema = createHydraCollectionSchema(userResponseSchema);
+
+export type UserCollectionResponse = z.infer<typeof userCollectionResponseSchema>;
+
+export const isUserCollectionResponse = (obj: unknown): obj is UserCollectionResponse => {
+  const isSuccess = userCollectionResponseSchema.safeParse(obj).success;
+
+  if (!isSuccess) {
+    console.trace("Invalid user collection response", obj);
+    return false;
+  }
+  return userCollectionResponseSchema.safeParse(obj).success;
+};
