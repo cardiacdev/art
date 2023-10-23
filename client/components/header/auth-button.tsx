3262eca03481af52839dc1988853ec2fd60debf9
Mark Loose <m.loose@boewa.de>
2023-10-23 09:28:27 +0200

    Wrap login and logout button display logic

---
+++ client/components/header/auth-button.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { useMeQuery } from "@/hooks/queries/use-me-query";
+
+import { Login } from "./login";
+import { Logout } from "./logout";
+
+export const AuthButton = () => {
+  const { data } = useMeQuery();
+
+  return data ? <Logout /> : <Login />;
+};
