08fdb890857d35a9dd1d65724c4c41c229cbca33
Mark Loose <m.loose@boewa.de>
2023-10-23 13:49:49 +0200

    Add `usePrevious` utility hook

---
+++ client/hooks/use-previous.ts
@@ -0,0 +1,15 @@
+"use client";
+
+import { useState } from "react";
+
+export function usePrevious(value: any) {
+  const [current, setCurrent] = useState(value);
+  const [previous, setPrevious] = useState(null);
+
+  if (value !== current) {
+    setPrevious(current);
+    setCurrent(value);
+  }
+
+  return previous;
+}
