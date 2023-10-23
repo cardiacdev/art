74ade9c6b391d8fd39204642de336f64038374f3
Mark Loose <m.loose@boewa.de>
2023-10-23 08:33:30 +0200

    Add titles to links

--- client/components/docs-link.tsx
+++ client/components/docs-link.tsx
@@ -8,6 +8,7 @@ export const DocsLink = () => {
       href={"/_profiler"}
       target="_blank"
       rel="noreferrer"
+      title="Open API Docs in new tab"
       className={"fixed bottom-3 left-3 z-50 h-12 w-12"}>
       <Icons.apiPlatform />
     </Link>
--- client/components/profiler-link.tsx
+++ client/components/profiler-link.tsx
@@ -7,6 +7,7 @@ export const ProfilerLink = () => {
     <Link
       href={"/_profiler"}
       target="_blank"
+      title="Open Symfony Profiler in new tab"
       rel="noreferrer"
       className={"fixed bottom-3 left-16 z-50 h-12 w-12"}>
       <Icons.symfony />
