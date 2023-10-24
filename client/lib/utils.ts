81a37f75c74e90e757f431e628bb4a2a7b40425d
Mark Loose <m.loose@boewa.de>
2023-10-24 11:45:56 +0200

    Add utility methods for form data serialization

--- client/lib/utils.ts
+++ client/lib/utils.ts
@@ -4,3 +4,29 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function falsyValueReplacer(key: string, value: any) {
+  if (!value) return undefined;
+  return value;
+}
+
+// See: https://github.com/orgs/react-hook-form/discussions/1991
+export function getDirtyFormValues<
+  DirtyFields extends Record<string, unknown>,
+  Values extends Record<keyof DirtyFields, unknown>,
+>(dirtyFields: DirtyFields, values: Values): Partial<typeof values> {
+  const dirtyValues = Object.keys(dirtyFields).reduce((prev, key) => {
+    // Unsure when RFH sets this to `false`, but omit the field if so.
+    if (!dirtyFields[key]) return prev;
+
+    return {
+      ...prev,
+      [key]:
+        typeof dirtyFields[key] === "object"
+          ? getDirtyFormValues(dirtyFields[key] as DirtyFields, values[key] as Values)
+          : values[key],
+    };
+  }, {});
+
+  return dirtyValues;
+}
