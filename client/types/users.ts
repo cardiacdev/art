8b46c804314ef4b0850970e3229f1a9fe5c1cbca
Mark Loose <m.loose@boewa.de>
2023-10-24 11:45:29 +0200

    Create typings for single entity responses

--- client/types/users.ts
+++ client/types/users.ts
@@ -2,6 +2,7 @@ import { z } from "zod";
 
 import { createHydraCollectionSchema, createHydraMemberSchema } from "./hydra";
 
+// ----- STANDARD FIELDS -----
 export const userResponseSchema = z.object({
   username: z.string(),
   email: z.string(),
@@ -9,6 +10,7 @@ export const userResponseSchema = z.object({
 
 export type UserResponse = z.infer<typeof userResponseSchema>;
 
+// ----- COLLECTIONS -----
 export const userCollectionResponseSchema = createHydraCollectionSchema(userResponseSchema);
 
 export type UserCollectionResponse = z.infer<typeof userCollectionResponseSchema>;
@@ -23,10 +25,39 @@ export const isUserCollectionResponse = (obj: unknown): obj is UserCollectionRes
   return true;
 };
 
+// ----- COLLECTION  MEMBERS -----
 export const userMemberSchema = createHydraMemberSchema(userResponseSchema);
 
 export type UserMember = z.infer<typeof userMemberSchema>;
 
+export const isUserMember = (obj: unknown): obj is UserMember => {
+  const isSuccess = userMemberSchema.safeParse(obj).success;
+
+  if (!isSuccess) {
+    console.warn("Invalid user member");
+    return false;
+  }
+  return true;
+};
+
+// ----- SINGLE RESPONSES (/entity/{id}) -----
+export const singleUserResponseSchema = userMemberSchema.extend({
+  "@context": z.string(),
+});
+
+export type SingleUserResponse = z.infer<typeof singleUserResponseSchema>;
+
+export const isSingleUserResponse = (obj: unknown): obj is SingleUserResponse => {
+  const isSuccess = singleUserResponseSchema.safeParse(obj).success;
+
+  if (!isSuccess) {
+    console.warn("Invalid single user response");
+    return false;
+  }
+  return true;
+};
+
+// ----- CUSTOM RESPONSE FROM /api/me ROUTE FOR AUTH CHECK -----
 export const meResponseSchema = z.object({
   id: z.number(),
   email: z.string(),
@@ -40,7 +71,7 @@ export const isMeResponse = (obj: unknown): obj is MeResponse => {
   const isSuccess = meResponseSchema.safeParse(obj).success;
 
   if (!isSuccess) {
-    console.trace("Invalid response", obj);
+    console.warn("Invalid response", obj);
     return false;
   }
   return true;
