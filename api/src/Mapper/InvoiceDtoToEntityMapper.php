3a20c6ed378e83af6fcddf81996e12539f6695a7
Mark Loose <m.loose@boewa.de>
2023-10-25 09:25:34 +0200

    Create mappers for invoice entity and dto

---
+++ api/src/Mapper/InvoiceDtoToEntityMapper.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Mapper;
+
+use App\ApiResource\InvoiceDto;
+use App\Entity\Client;
+use App\Entity\Invoice;
+use App\Repository\InvoiceRepository;
+use Exception;
+use Symfonycasts\MicroMapper\AsMapper;
+use Symfonycasts\MicroMapper\MapperInterface;
+use Symfonycasts\MicroMapper\MicroMapperInterface;
+
+use function assert;
+
+#[AsMapper(from: InvoiceDto::class, to: Invoice::class)]
+class InvoiceDtoToEntityMapper implements MapperInterface
+{
+    public function __construct(
+        private InvoiceRepository $invoiceRepository,
+        private MicroMapperInterface $microMapper
+    ) {
+    }
+
+    public function load(object $from, string $toClass, array $context = []): object
+    {
+        $dto = $from;
+        assert($dto instanceof InvoiceDto);
+
+        $entity = $dto->id ? $this->invoiceRepository->find($dto->id) : new Invoice();
+        if (!$entity) {
+            throw new Exception(sprintf('Entity %d not found', $dto->id));
+        }
+
+        return $entity;
+    }
+
+    public function populate(object $from, object $to, array $context = []): object
+    {
+        $dto = $from;
+        assert($dto instanceof InvoiceDto);
+        $entity = $to;
+        assert($entity instanceof Invoice);
+
+        $entity->setInvoiceNumber($dto->invoiceNumber);
+        $entity->setBillingDate($dto->billingDate);
+        $entity->setRemarks($dto->remarks);
+
+        $entity->setClient($this->microMapper->map($dto->client, Client::class));
+
+        return $entity;
+    }
+}
---
+++ api/src/Mapper/InvoiceEntityToDtoMapper.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Mapper;
+
+use App\ApiResource\ClientDto;
+use App\ApiResource\InvoiceDto;
+use App\Entity\Invoice;
+use Symfonycasts\MicroMapper\AsMapper;
+use Symfonycasts\MicroMapper\MapperInterface;
+use Symfonycasts\MicroMapper\MicroMapperInterface;
+
+use function assert;
+
+#[AsMapper(from: Invoice::class, to: InvoiceDto::class)]
+class InvoiceEntityToDtoMapper implements MapperInterface
+{
+    public function __construct(
+        private MicroMapperInterface $microMapper
+    ) {
+    }
+
+    public function load(object $from, string $toClass, array $context = []): object
+    {
+        $entity = $from;
+        assert($entity instanceof Invoice);
+
+        $dto = new InvoiceDto();
+        $dto->id = $entity->getId();
+
+        return $dto;
+    }
+
+    public function populate(object $from, object $to, array $context = []): object
+    {
+        $entity = $from;
+        assert($entity instanceof Invoice);
+        $dto = $to;
+        assert($dto instanceof InvoiceDto);
+
+        $dto->invoiceNumber = $entity->getInvoiceNumber();
+        $dto->billingDate = $entity->getBillingDate();
+        $dto->remarks = $entity->getRemarks();
+
+        $dto->client = $this->microMapper->map($entity->getClient(), ClientDto::class);
+
+        // TODO - Invoice Items
+
+        return $dto;
+    }
+}
