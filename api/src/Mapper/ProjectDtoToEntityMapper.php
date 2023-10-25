bdf41332568011a17e83dff9070941e387401075
Mark Loose <m.loose@boewa.de>
2023-10-25 09:16:18 +0200

    Create mappers for project entity and dto

---
+++ api/src/Mapper/ProjectDtoToEntityMapper.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Mapper;
+
+use App\ApiResource\ProjectDto;
+use App\Entity\Client;
+use App\Entity\Project;
+use App\Repository\ProjectRepository;
+use Exception;
+use Symfonycasts\MicroMapper\AsMapper;
+use Symfonycasts\MicroMapper\MapperInterface;
+use Symfonycasts\MicroMapper\MicroMapperInterface;
+
+use function assert;
+
+#[AsMapper(from: ProjectDto::class, to: Project::class)]
+class ProjectDtoToEntityMapper implements MapperInterface
+{
+    public function __construct(
+        private ProjectRepository $projectRepository,
+        private MicroMapperInterface $microMapper
+    ) {
+    }
+
+    public function load(object $from, string $toClass, array $context = []): object
+    {
+        $dto = $from;
+        assert($dto instanceof ProjectDto);
+
+        $entity = $dto->id ? $this->projectRepository->find($dto->id) : new Project();
+        if (!$entity) {
+            throw new Exception(sprintf('Entity %d not found', $dto->id));
+        }
+
+        return $entity;
+    }
+
+    public function populate(object $from, object $to, array $context = []): object
+    {
+        $dto = $from;
+        assert($dto instanceof ProjectDto);
+        $entity = $to;
+        assert($entity instanceof Project);
+
+        $entity->setName($dto->name);
+        $entity->setHourlyRate($dto->hourlyRate);
+
+        $entity->setClient($this->microMapper->map($dto->client, Client::class));
+
+        return $entity;
+    }
+}
---
+++ api/src/Mapper/ProjectEntityToDtoMapper.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Mapper;
+
+use App\ApiResource\ClientDto;
+use App\ApiResource\ProjectDto;
+use App\Entity\Project;
+use Symfonycasts\MicroMapper\AsMapper;
+use Symfonycasts\MicroMapper\MapperInterface;
+use Symfonycasts\MicroMapper\MicroMapperInterface;
+
+use function assert;
+
+#[AsMapper(from: Project::class, to: ProjectDto::class)]
+class ProjectEntityToDtoMapper implements MapperInterface
+{
+    public function __construct(
+        private MicroMapperInterface $microMapper
+    ) {
+    }
+
+    public function load(object $from, string $toClass, array $context = []): object
+    {
+        $entity = $from;
+        assert($entity instanceof Project);
+
+        $dto = new ProjectDto();
+        $dto->id = $entity->getId();
+
+        return $dto;
+    }
+
+    public function populate(object $from, object $to, array $context = []): object
+    {
+        $entity = $from;
+        assert($entity instanceof Project);
+        $dto = $to;
+        assert($dto instanceof ProjectDto);
+
+        $dto->name = $entity->getName();
+        $dto->hourlyRate = $entity->getHourlyRate();
+        $dto->client = $this->microMapper->map($entity->getClient(), ClientDto::class);
+
+        return $dto;
+    }
+}
