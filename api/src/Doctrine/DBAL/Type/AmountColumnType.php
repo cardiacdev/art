bb261ee518cf210162374bfaf43a879ee5a3def3
Mark Loose <m.loose@boewa.de>
2023-11-06 15:02:28 +0100

    Add custom Doctrine DBAL types for decimals

---
+++ api/src/Doctrine/DBAL/Type/AmountColumnType.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Doctrine\DBAL\Type;
+
+use App\Model\Decimal;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\Type;
+use Symfony\Component\Validator\Exception\UnexpectedTypeException;
+
+class AmountColumnType extends Type
+{
+    public const TYPE_NAME = 'amount';
+
+    public function getSQLDeclaration(array $fieldDeclaration, AbstractPlatform $platform): string
+    {
+        return $platform->getDecimalTypeDeclarationSQL([
+            'precision' => 14,
+            'scale' => 2,
+        ] + $fieldDeclaration);
+    }
+
+    public function convertToDatabaseValue($value, AbstractPlatform $platform): ?string
+    {
+        if (null !== $value) {
+            if (!$value instanceof Decimal) {
+                throw new UnexpectedTypeException($value, Decimal::class);
+            }
+
+            return $value->getValue();
+        }
+
+        return null;
+    }
+
+    public function convertToPHPValue($value, AbstractPlatform $platform): ?Decimal
+    {
+        return null !== $value
+            ? new Decimal((string) $value)
+            : null;
+    }
+
+    public function requiresSQLCommentHint(AbstractPlatform $platform): bool
+    {
+        return true;
+    }
+
+    public function getName(): string
+    {
+        return static::TYPE_NAME;
+    }
+}
