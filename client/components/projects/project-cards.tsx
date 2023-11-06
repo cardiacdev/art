6a79cef1e503be645d101f0b5edb3ee92381491d
Mark Loose <m.loose@boewa.de>
2023-11-06 16:03:48 +0100

    Paginate project cards view

--- client/components/projects/project-cards.tsx
+++ client/components/projects/project-cards.tsx
@@ -1,19 +1,39 @@
 "use client";
 
+import { useState } from "react";
+
 import { useProjectsQuery } from "@/hooks/queries/projects/use-projects-query";
 
+import { PaginationControls } from "../ui/pagination-controls";
 import { ProjectCard } from "./project-card";
 
 export const ProjectCards = () => {
-  const { data } = useProjectsQuery();
+  const [page, setPage] = useState(1);
+  const searchParams = new URLSearchParams();
+
+  searchParams.append("itemsPerPage", "12");
+  searchParams.append("page", `${page}`);
+  const { data } = useProjectsQuery(searchParams);
 
   if (!data) return null;
 
+  const pageCount = Math.ceil(data["hydra:totalItems"] / 12);
+
   return (
-    <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
-      {data["hydra:member"].map((project) => (
-        <ProjectCard key={project["@id"]} project={project} />
-      ))}
-    </div>
+    <>
+      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+        {data["hydra:member"].map((project) => (
+          <ProjectCard key={project["@id"]} project={project} />
+        ))}
+      </div>
+      <PaginationControls
+        page={page}
+        pageCount={pageCount}
+        first={() => setPage(1)}
+        previous={() => setPage(page - 1)}
+        next={() => setPage(page + 1)}
+        last={() => setPage(pageCount)}
+      />
+    </>
   );
 };
