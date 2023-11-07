6d3ab596fad79ddfc8ba010b505320e23044ac5c
Mark Loose <m.loose@boewa.de>
2023-11-07 16:54:43 +0100

    Add delete client functionality to client header

--- client/components/clients/id/client-header.tsx
+++ client/components/clients/id/client-header.tsx
@@ -1,17 +1,40 @@
 "use client";
 
 import { useSingleClientQuery } from "@/hooks/queries/clients/use-single-client-query";
+import { useModal } from "@ebay/nice-modal-react";
+import { Pencil1Icon, TrashIcon } from "@radix-ui/react-icons";
+
+import { Button } from "@/components/ui/button";
+
+import { DeleteClientDialog } from "./delete-client-dialog";
 
 interface ClientHeadingProps {
   id: string;
 }
 
 export const ClientHeader = ({ id }: ClientHeadingProps) => {
-  const { data } = useSingleClientQuery(id);
+  const { data: client } = useSingleClientQuery(id);
+  const deleteModal = useModal(DeleteClientDialog, { client });
+
+  if (!client) return null;
 
   return (
-    <h1 className="text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl">
-      {data?.name}
-    </h1>
+    <div className="flex items-center justify-between">
+      <h1 className="text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl">
+        {client.name}
+      </h1>
+      <div className="flex gap-2">
+        <Button variant="ghost" size="icon" className="ml-auto" onClick={() => null}>
+          <Pencil1Icon className="h-7 w-7" />
+        </Button>
+        <Button
+          variant="ghost"
+          size="icon"
+          className="ml-auto text-destructive"
+          onClick={() => deleteModal.show({ client })}>
+          <TrashIcon className="h-7 w-7" />
+        </Button>
+      </div>
+    </div>
   );
 };
