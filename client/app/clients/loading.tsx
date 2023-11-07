6bdd5d13f6b597b2c41342cbcc94ad06ea03ecb0
Mark Loose <m.loose@boewa.de>
2023-11-07 11:06:43 +0100

    Show cards skeleton on clients loading page

--- client/app/clients/loading.tsx
+++ client/app/clients/loading.tsx
@@ -1,5 +1,12 @@
-import { UserTableSkeleton } from "@/components/skeleton/user-table-skeleton";
+import { ProjectCardsSkeleton } from "@/components/skeleton/project-cards-skeleton";
 
 export default function Loading() {
-  return <UserTableSkeleton />;
+  return (
+    <main className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
+      <h1 className="text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl">
+        Kunden
+      </h1>
+      <ProjectCardsSkeleton />
+    </main>
+  );
 }
