bc5a31c7e4e7dbf47242af72631c0d3d81bad757
Mark Loose <m.loose@boewa.de>
2023-11-09 16:09:11 +0100

    Revert narrowing project stats cards

--- client/components/projects/id/project-stats.tsx
+++ client/components/projects/id/project-stats.tsx
@@ -17,7 +17,7 @@ export const ProjectStats = ({ id }: ProjectStatsProps) => {
   return (
     <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
       <Card>
-        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2 pt-4">
+        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
           <CardTitle className="text-sm font-medium">Stundensatz</CardTitle>
           <ClockIcon className="h-4 w-4 text-muted-foreground" />
         </CardHeader>
@@ -25,33 +25,37 @@ export const ProjectStats = ({ id }: ProjectStatsProps) => {
           <div className="text-2xl font-bold">
             {project?.hourlyRate ? `€${dotToComma(project?.hourlyRate)}/h` : "-"}
           </div>
+          <p className="text-xs text-muted-foreground">Editierbar in Einstellungen</p>
         </CardContent>
       </Card>
       <Card>
-        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2 pt-4">
+        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
           <CardTitle className="text-sm font-medium">Offene Aufgaben</CardTitle>
           <EyeOpenIcon className="h-4 w-4 text-muted-foreground" />
         </CardHeader>
         <CardContent>
           <div className="text-2xl font-bold">{project?.openTasks}</div>
+          <p className="text-xs text-muted-foreground">Nicht vollständig abgerechnet</p>
         </CardContent>
       </Card>
       <Card>
-        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2 pt-4">
+        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
           <CardTitle className="text-sm font-medium">Noch nicht abgerechnet</CardTitle>
           <Icons.euro className="h-4 w-4 text-muted-foreground" />
         </CardHeader>
         <CardContent>
           <div className="text-2xl font-bold">€{dotToComma(project?.notBilledAmount)}</div>
+          <p className="text-xs text-muted-foreground">Aus Rechnungsposten</p>
         </CardContent>
       </Card>
       <Card>
-        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2 pt-4">
+        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
           <CardTitle className="text-sm font-medium">Abgerechnet</CardTitle>
           <Icons.euro className="h-4 w-4 text-muted-foreground" />
         </CardHeader>
         <CardContent>
           <div className="text-2xl font-bold">€{dotToComma(project?.billedAmount)}</div>
+          <p className="text-xs text-muted-foreground">Aus Rechnungsposten</p>
         </CardContent>
       </Card>
     </div>
