c9fd52dc816f7664deddf929c9237ac96d64f59d
Mark Loose <m.loose@boewa.de>
2023-11-09 12:17:34 +0100

    Add query keys for subresource

--- client/hooks/queries/projects/projects-query-key-factory.ts
+++ client/hooks/queries/projects/projects-query-key-factory.ts
@@ -4,5 +4,7 @@ import { URLSearchParamsToObj } from "@/lib/utils";
 export const projectsKeys = {
   all: ["projects"] as const,
   allWithParams: (params: SearchParams) => ["projects", { params: URLSearchParamsToObj(params) }] as const,
+  tasksWithParams: (id: string, params: SearchParams) =>
+    ["projects", id, "tasks", { params: URLSearchParamsToObj(params) }] as const,
   single: (id: string) => ["projects", id] as const,
 };
