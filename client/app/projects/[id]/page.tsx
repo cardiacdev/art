40a0259d3677a73ba191d425f9adbeb24f768b99
Mark Loose <m.loose@boewa.de>
2023-11-09 14:39:58 +0100

    Delegate content negotiation to header

--- client/app/projects/[id]/page.tsx
+++ client/app/projects/[id]/page.tsx
@@ -3,15 +3,28 @@ import { projectsKeys } from "@/hooks/queries/projects/projects-query-key-factor
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 
 import { isSingleProjectResponse } from "@/types/projects";
+import { isTaskCollectionResponse } from "@/types/tasks";
+import { SearchParams } from "@/types/utils";
 import { fetchJsonLd } from "@/lib/fetch/fetch-json-ld";
 import { createQueryClient } from "@/lib/query-client";
 import { ProjectHeader } from "@/components/projects/id/project-header";
-import { ProjectStats } from "@/components/projects/id/project-stats";
 
 const fetchProject = async (id: string) => {
   const data = await fetchJsonLd(`${env.NEXT_PUBLIC_API_URL}/api/projects/${id}`);
 
   if (!isSingleProjectResponse(data)) throw new Error("Invalid response");
+
+  return data;
+};
+
+const fetchTasksByProject = async (id: string, params: SearchParams) => {
+  const searchParams = new URLSearchParams(params);
+
+  const data = await fetchJsonLd(
+    `${env.NEXT_PUBLIC_API_URL}/api/projects/${id}/tasks?${searchParams.toString()}`,
+  );
+
+  if (!isTaskCollectionResponse(data)) throw new Error("Invalid response");
 
   return data;
 };
@@ -23,16 +36,24 @@ interface PageProps {
 export default function Page({ params: { id } }: PageProps) {
   const queryClient = createQueryClient();
 
+  // Prefetch project
   queryClient.prefetchQuery({
     queryKey: projectsKeys.single(id),
     queryFn: () => fetchProject(id),
+  });
+
+  // Prefetch tasks for project
+  const params = new URLSearchParams();
+  params.append("page", "1");
+  queryClient.prefetchQuery({
+    queryKey: projectsKeys.tasksWithParams(id, params),
+    queryFn: () => fetchTasksByProject(id, params),
   });
 
   return (
     <main className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <HydrationBoundary state={dehydrate(queryClient)}>
         <ProjectHeader id={id} />
-        <ProjectStats id={id} />
       </HydrationBoundary>
     </main>
   );
